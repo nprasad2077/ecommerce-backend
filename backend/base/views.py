58c3192790654c92b0397be61d1657c344ea5df8
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-02-16 18:36:56 -0600

    REST response

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .products import products
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
 
+@api_view(['GET'])
 def getRoutes(request):
     routes = [
         '/api/products/',
@@ -16,7 +19,7 @@ def getRoutes(request):
         '/api/products/<update>/<id>/'
 
     ]
-    return JsonResponse(routes, safe=False)
+    return Response(routes)
 
 def getProducts(request):
     return JsonResponse(products, safe=False)
