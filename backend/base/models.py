e0e98c999d77b04c78c13d04d95b529c2380caf5
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-02-23 13:06:29 -0600

    rating model;

--- backend/base/models.py
+++ backend/base/models.py
@@ -18,4 +18,17 @@ class Product(models.Model):
     _id = models.AutoField(primary_key=True, editable=False)
 
     def __str__(self):
-        return self.name 
+        return self.name
+    
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    name = category = models.CharField(max_length=200, null=True, blank=True)
+    rating = models.IntegerField(null=True, blank=True, default=0)
+    comment = models.TextField(null=True, blank=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.rating)
+
