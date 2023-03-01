8dfe783260dc47f2ebfa672e546bb0ba8eb4844a
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-02-28 20:52:43 -0600

    return name JWT

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -2,6 +2,19 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import Product
 
+class UserSerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField(read_only = True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'name']
+
+    def get_name(self, obj):
+        name = obj.first_name
+        if name == '':
+            name = obj.email
+        return name
+
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
--- backend/base/views.py
+++ backend/base/views.py
@@ -4,7 +4,7 @@ from .products import products
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Product
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, UserSerializer
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -38,6 +38,13 @@ def getRoutes(request):
 
     ]
     return Response(routes)
+
+
+@api_view(['GET'])
+def getUserProfile(request):
+    user = request.user
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
 
 
 @api_view(['GET'])
