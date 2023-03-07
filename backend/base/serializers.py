ef7010f6885ccf7f7b5833a7e747c030b8a4a490
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-03-07 12:19:41 -0600

    typo fix

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -44,7 +44,7 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class ShippinAddressSerializer(serializers.ModelSerializer):
+class ShippingAddressSerializer(serializers.ModelSerializer):
     class Meta:
         model = ShippingAddress
         fields = '__all__'
