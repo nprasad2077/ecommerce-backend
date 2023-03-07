ef734d2546c1f2da78834eaf959a793cb3bf16f8
nprasad2077 <114438461+nprasad2077@users.noreply.github.com>
2023-03-06 19:28:34 -0600

    backend modify

--- backend/base/views/order_views.py
+++ backend/base/views/order_views.py
@@ -5,7 +5,7 @@ from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
 from base.models import Product, Order, OrderItem, ShippingAddress
-from base.serializers import ProductSerializer
+from base.serializers import ProductSerializer, OrderSerializer
 
 from rest_framework import status
 
@@ -59,4 +59,5 @@ def addOrderItems(request):
         product.countInStock -= item.qty
         product.save()
 
-    return Response('Order')
+    serializer = OrderSerializer(order, many=True)
+    return Response(serializer.data)
